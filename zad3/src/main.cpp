902ea0d262dd06d1a242ab672769cde8a14f8e70
Jedrzej <254898@student.pwr.edu.pl>
2021-04-12 19:12:16 +0200

    Poprawiono bledy

--- zad3/src/main.cpp
+++ zad3/src/main.cpp
@@ -4,112 +4,14 @@
 #include "Wektor2D.hh"
 #include "Macierz2x2.hh"
 #include "Prostokat.hh"
-//#include "lacze_do_gnuplota.hh"
 #include "Dr2D_gnuplot_api.hh"
 
 using namespace std;
 
-/*
- * Tu definiujemy pozostale funkcje.
- * Lepiej jednak stworzyc dodatkowy modul
- * i tam je umiescic. Ten przyklad pokazuje
- * jedynie absolutne minimum.
- */
 
 #define PRZESUN 1
 #define DL_KROTKI_BOK 5
 #define DL_DLUGI_BOK 10
-
-/*!
- * Przyklad zapisu wspolrzednych zbioru punktow do strumienia wyjściowego.
- * Dane sa odpowiednio sformatowane, tzn. przyjęto notację stałoprzecinkową
- * z dokładnością do 10 miejsca po przecinku. Szerokość wyświetlanego pola 
- * to 16 miejsc, sposób wyrównywania - do prawej strony.
- * \param[in] StrmWy - strumien wyjsciowy, do ktorego maja zostac zapisane
- *                     kolejne wspolrzedne.
- * \param[in] Przesuniecie - ten parameter jest tylko po to, aby pokazać
- *                          mozliwosc zmiany wspolrzednych i prostokata
- *                          i zmiane jego polorzenia na okienku graficznym
- *                         rysownym przez gnuplota.
- * \retval true - gdy operacja zapisu powiodła się,
- * \retval false - w przypadku przeciwnym.
- */
-/*void PrzykladZapisuWspolrzednychDoStrumienia( ostream&     StrmWy, 
-                                              double       Przesuniecie
-                                            )
-{
-   //---------------------------------------------------------------
-   // To tylko przyklad !!!
-   // W programie nalezy uzywać pojęcia wektora, a nie oddzielnych 
-   // zmiennych do reprezentowania wspolrzednych!
-   //
-  double  x1, y1, x2, y2, x3, y3, x4, y4; 
-
-  x1 = y1 = 10;
-  x2 = x1 + DL_DLUGI_BOK;  y2 = y1;
-  x3 = x2;  y3 = y2 + DL_KROTKI_BOK;
-  x4 = x3 - DL_DLUGI_BOK; y4 = y3;
-
-
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+Przesuniecie 
-         << setw(16) << fixed << setprecision(10) << y1+Przesuniecie << endl;
-  StrmWy << setw(16) << fixed << setprecision(10) << x2+Przesuniecie 
-         << setw(16) << fixed << setprecision(10) << y2+Przesuniecie << endl;
-  StrmWy << setw(16) << fixed << setprecision(10) << x3+Przesuniecie 
-         << setw(16) << fixed << setprecision(10) << y3+Przesuniecie << endl;
-  StrmWy << setw(16) << fixed << setprecision(10) << x4+Przesuniecie 
-         << setw(16) << fixed << setprecision(10) << y4+Przesuniecie << endl;
-  StrmWy << setw(16) << fixed << setprecision(10) << x1+Przesuniecie 
-         << setw(16) << fixed << setprecision(10) << y1+Przesuniecie << endl; 
-                             // Jeszcze raz zapisujemy pierwszy punkt,
-                             // aby gnuplot narysowal zamkniętą linię.
-
-
-
-	Wektor2D w1, w2, w3, w4, przesun(Przesuniecie, Przesuniecie);
-
-	w1[0] = w1[1] = 10;
-	w2[0] = w1[0] + DL_DLUGI_BOK;  w2[1] = w1[1];
-	w3[0] = w2[0];  w3[1] = w2[1] + DL_KROTKI_BOK;
-	w4[0] = w3[0] - DL_DLUGI_BOK; w4[1] = w3[1];
-
-	StrmWy << w1+przesun;
-	StrmWy << w2+przesun;
-	StrmWy << w3+przesun;
-	StrmWy << w4+przesun;
-	StrmWy << w1+przesun;
-}*/
-
-/*!
- * Przyklad zapisu wspolrzednych zbioru punktow do pliku, z ktorego
- * dane odczyta program gnuplot i narysuje je w swoim oknie graficznym.
- * \param[in] sNazwaPliku - nazwa pliku, do którego zostana zapisane
- *                          wspolrzędne punktów.
- * \param[in] Przesuniecie - ten parameter jest tylko po to, aby pokazać
- *                          mozliwosc zmiany wspolrzednych i prostokata
- *                          i zmiane jego polorzenia na okienku graficznym
- *                         rysownym przez gnuplota.
- * \retval true - gdy operacja zapisu powiodła się,
- * \retval false - w przypadku przeciwnym.
- */
-/*bool PrzykladZapisuWspolrzednychDoPliku( const char  *sNazwaPliku,
-                                         double       Przesuniecie
-                                       )
-{
-  ofstream  StrmPlikowy;
-
-  StrmPlikowy.open(sNazwaPliku);
-  if (!StrmPlikowy.is_open())  {
-    cerr << ":(  Operacja otwarcia do zapisu \"" << sNazwaPliku << "\"" << endl
-	 << ":(  nie powiodla sie." << endl;
-    return false;
-  }
-
-  PrzykladZapisuWspolrzednychDoStrumienia(StrmPlikowy,Przesuniecie);
-
-  StrmPlikowy.close();
-  return !StrmPlikowy.fail();
-}*/
 
 int main()
 {
@@ -117,20 +19,11 @@ int main()
 	//rysownik->change_ref_time_ms(0);
 
 	//Tworzenie prostokata na ktorym beda wykonywane operacje
-	Wektor2D w1, w2, w3, w4;
-
-	w1[0] = w1[1] = 1;
-	w2[0] = w1[0] + DL_DLUGI_BOK;
-	w2[1] = w1[1];
-	w3[0] = w2[0];
-	w3[1] = w2[1] + DL_KROTKI_BOK;
-	w4[0] = w3[0] - DL_DLUGI_BOK;
-	w4[1] = w3[1];
-
 	Prostokat Pr(Wektor2D(PRZESUN,PRZESUN), Wektor2D(PRZESUN + DL_DLUGI_BOK,PRZESUN), Wektor2D(PRZESUN + DL_DLUGI_BOK,PRZESUN + DL_KROTKI_BOK), Wektor2D(PRZESUN,PRZESUN + DL_KROTKI_BOK));
 	Prostokat tmp = Pr;
+	//Pr.rysuj(rysownik);
 
-	//std::cout << "Wspolrzedne poczatkowego prostokata\n" << Pr << std::endl;
+	std::cout << "Wspolrzedne poczatkowego prostokata\n" << Pr << std::endl;
 
 	std::cout << "Menu:";
 
@@ -169,9 +62,9 @@ int main()
 				std::cout << "Niepoprawny format danych\n";
 				continue;
 			}
-			//std::cout << MacierzObr2x2(deg*powtorzenia) << std::endl;
-			//for(int i = 0; i < powtorzenia; i++)
-			tmp = tmp.rotacja(MacierzObr2x2(deg*powtorzenia));
+			/*for(int i = 0; i < powtorzenia; i++)
+				tmp = tmp.rotacja(MacierzObr2x2(deg));*/
+			tmp = tmp.rotacja(MacierzObr2x2(deg*powtorzenia)); //Przy 10^15 sin/cos zaczynaja sie rozjezdzac
 			break;
 		case 2: //Przesuniecie
 			{
@@ -198,7 +91,7 @@ int main()
 			continue;
 		}
 
-		std::cout << "Roznica dlugosci bokow wzgledem oryginalu\n";
+		std::cout << "Roznica dlugosci bokow wzgledem oryginalu:\n";
 		std::cout << "|P1 P2| " << (tmp[0]-tmp[1]).dlugosc() - (Pr[0]-Pr[1]).dlugosc() << std::endl;
 		std::cout << "|P2 P3| " << (tmp[1]-tmp[2]).dlugosc() - (Pr[1]-Pr[2]).dlugosc() << std::endl;
 		std::cout << "|P3 P4| " << (tmp[2]-tmp[3]).dlugosc() - (Pr[2]-Pr[3]).dlugosc() << std::endl;
