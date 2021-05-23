54bfe9aa2e2474805bf49bc9ad8782c1668363ee
Jedrzej <254898@student.pwr.edu.pl>
2021-05-23 22:25:53 +0200

    Poprawniono komentarze

--- zad5/inc/PlaskowyzProst.hh
+++ zad5/inc/PlaskowyzProst.hh
@@ -4,13 +4,24 @@
 #include "InterfejsElemPowierzchni.hh"
 #include "Prostopadloscian.hh"
 
+//! Modeluje klase plaskowyzu prostopadlosciennego
 class PlaskowyzProst : public Prostopadloscian//, public InterfejsElemPowierzchni
 {
 private:
     //! Prostopadloscian tworzacy plaskowyz prostopadloscienny
     //Prostopadloscian plasProst;
 public:
-    PlaskowyzProst(Wektor<3> bazS, MacierzObr<3> bazO, UkladW * wsk, std::shared_ptr<drawNS::Draw3DAPI> rys, std::string col, double dlugosc, double szerokosc, double wysokosc) : Prostopadloscian(bazS+Wektor<3>{0,0,wysokosc/2}, bazO, wsk, rys, col, dlugosc, szerokosc, wysokosc) {}
+    /*! Konstruktor tworzacy plaskowyz prostopadloscienny
+     * \param[in] bazS - poczatkowy srodek lokalnego ukladu wspolrzednych
+     * \param[in] bazO - poczatkwowa orientacja
+     * \param[in] wsk - wskaznik na rodzica
+     * \param[in] rys - wskaznik na rysownik APIGnuPlot3D
+     * \param[in] col - kolor rysowanego tworzonego elementu wykorzystywany przy rysowaniu
+     * \param[in] dlugosc - dlugosc (Y) krawedzi prostopadloscianu
+     * \param[in] szerokosc - szerokosc (X) krawedzi prostopadloscianu
+     * \param[in] wysokosc - wysokosc (Z) prostopadloscianu
+     */
+    PlaskowyzProst(Wektor<3> bazS, MacierzObr<3> bazO, std::shared_ptr<drawNS::Draw3DAPI> rys, std::string col, double dlugosc, double szerokosc, double wysokosc) : Prostopadloscian(bazS+Wektor<3>{0,0,wysokosc/2}, bazO, nullptr, rys, col, dlugosc, szerokosc, wysokosc) {}
     //bool czy_nad() override;
     //bool czy_ladowac() override;
 };
